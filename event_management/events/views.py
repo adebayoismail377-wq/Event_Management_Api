66b80a22a25f91b4cfab5a267487d08b4de545b0
Adebayo Ismail <adebayoismail377@gmail.com>
2026-02-27 12:50:44 +0100

    indentation issues resolved

--- event_management/events/views.py
+++ event_management/events/views.py
@@ -16,29 +16,24 @@ class EventViewSet(viewsets.ModelViewSet):
     serializer_class = EventSerializer
     permission_classes = [IsAuthenticated]
 
-    # Users only see their own events
     def get_queryset(self):
         if self.request.user.is_authenticated:
             return Event.objects.filter(organizer=self.request.user)
         return Event.objects.none()
 
-    # Automatically assign logged-in user
     def perform_create(self, serializer):
         serializer.save(organizer=self.request.user)
 
-    # Only allow owner to update
     def perform_update(self, serializer):
         if serializer.instance.organizer != self.request.user:
             raise PermissionDenied("You can only edit your own events.")
         serializer.save()
 
-    # Only allow owner to delete
     def perform_destroy(self, instance):
         if instance.organizer != self.request.user:
             raise PermissionDenied("You can only delete your own events.")
         instance.delete()
 
-    # Public upcoming events
     @action(detail=False, methods=['get'], permission_classes=[AllowAny])
     def upcoming(self, request):
         events = Event.objects.filter(event_datetime__gt=timezone.now())
@@ -57,24 +52,25 @@ class EventViewSet(viewsets.ModelViewSet):
         if start_date and end_date:
             events = events.filter(event_datetime__range=[start_date, end_date])
 
+        page = self.paginate_queryset(events)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
         serializer = self.get_serializer(events, many=True)
         return Response(serializer.data)
 
-    # Register for event
     @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
     def register(self, request, pk=None):
         event = self.get_object()
 
-        # Already registered
         if request.user in event.attendees.all():
             return Response(
                 {"message": "You are already registered."},
                 status=status.HTTP_400_BAD_REQUEST
             )
 
-        # If event is full
         if event.is_full():
-            # Already on waitlist
             if request.user in event.waitlist.all():
                 return Response(
                     {"message": "You are already on the waitlist."},
@@ -82,21 +78,17 @@ class EventViewSet(viewsets.ModelViewSet):
                 )
 
             event.waitlist.add(request.user)
-
             return Response(
                 {"message": "Event is full. You have been added to the waitlist."},
                 status=status.HTTP_200_OK
             )
 
-        # Otherwise register normally
         event.attendees.add(request.user)
-
         return Response(
-            {"message": "Successfully registered for the event!"},
+            {"message": "Successfully registered!"},
             status=status.HTTP_200_OK
         )
 
-    # Cancel registration
     @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
     def cancel_registration(self, request, pk=None):
         event = self.get_object()
@@ -104,7 +96,6 @@ class EventViewSet(viewsets.ModelViewSet):
         if request.user in event.attendees.all():
             event.attendees.remove(request.user)
 
-            # Auto promote from waitlist
             if event.waitlist.exists():
                 next_user = event.waitlist.first()
                 event.waitlist.remove(next_user)
