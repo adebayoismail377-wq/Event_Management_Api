0b5df7886ac198b17a50177d85ffb7352c1ab7d1
Adebayo Ismail <adebayoismail377@gmail.com>
2026-03-07 22:01:02 +0100

    Add API homepage

--- event_management/event_management/urls.py
+++ event_management/event_management/urls.py
@@ -16,10 +16,30 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+from rest_framework.routers import DefaultRouter
+from events.views import EventViewSet, UserViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+
+
+router = DefaultRouter()
+router.register(r'events', EventViewSet)
+router.register(r'users', UserViewSet)
+
+
+def api_home(request):
+    return JsonResponse({
+        "message": "Welcome to Event Management API",
+        "available_endpoints": {
+            "admin": "/admin/",
+            "events": "/events/",
+            "users": "/users/"
+        }
+    })
+
 
 urlpatterns = [
     # Admin
