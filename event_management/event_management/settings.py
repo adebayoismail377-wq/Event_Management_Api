94b9ae5ab1d175b6400fa78f560468e44ee0ed9a
Adebayo Ismail <adebayoismail377@gmail.com>
2026-03-08 16:18:01 +0100

    switch to sql database on render

--- event_management/event_management/settings.py
+++ event_management/event_management/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'event_db',
-        'USER': 'root',
-        'PASSWORD': 'Luminousphotography;1',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
